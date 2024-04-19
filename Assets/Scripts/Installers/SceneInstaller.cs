09077ff545652fdcb27f1197f5135f19607f845d
Muhammed Said Uraz <msaiduraz@gmail.com>
2024-04-19 19:55:20 +0300

    Installer arrangement

--- Assets/Scripts/Installers/SceneInstaller.cs
+++ Assets/Scripts/Installers/SceneInstaller.cs
@@ -1,13 +1,21 @@
 using Zenject;
+using UnityEngine;
 using Assets.Scripts.GridModule;
 using Assets.Scripts.InputModule;
 using Assets.Scripts.MatchModule;
 using Assets.Scripts.TimerModule;
+using Grid = Assets.Scripts.GridModule.Grid;
 
 namespace Assets.Scripts.Installers
 {
     public class SceneInstaller : MonoInstaller<SceneInstaller>
     {
+        #region Variables
+
+        [SerializeField] private Grid _gridPrefab;
+
+        #endregion Variables
+
         #region Functions
 
         public override void InstallBindings()
@@ -19,6 +27,9 @@ namespace Assets.Scripts.Installers
             Container.BindInterfacesAndSelfTo<Timer>().AsTransient();
             Container.BindInterfacesAndSelfTo<CustomInput>().AsSingle();
 
+            Container.BindMemoryPool<Grid, GridPool>().FromComponentInNewPrefab(_gridPrefab);
+            Container.BindInterfacesAndSelfTo<GridCreator>().AsSingle();
+            Container.BindInterfacesAndSelfTo<GridManager>().AsSingle();
             Container.BindInterfacesAndSelfTo<GridSelector>().AsSingle();
 
             Container.BindInterfacesAndSelfTo<MatchOperation>().AsSingle();
